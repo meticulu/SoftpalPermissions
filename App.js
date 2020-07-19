6abb2684419c80fa2d3554d0ea95af020c01da44
meticulu <113519094+meticulu@users.noreply.github.com>
2020-07-19 19:45:10 +0000

    Fix validation errors.

--- App.js
+++ App.js
@@ -38,4 +38,26 @@ export default class App extends React.Component {
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this 
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+# AudioScribe
+Tired of taking notes? Hands cramping during lectures?
+AudioScribe to the rescue!
+
+## About
+After coming out of violin 
