27d3892897446825e36c839f5dfe0282e2af9e4d
meticulu <113519094+meticulu@users.noreply.github.com>
2020-07-19 18:04:26 +0000

    synchronize with shell-skeleton

--- App.js
+++ App.js
@@ -18,3 +18,24 @@ export default class App extends React.Component {
           onFinish={this._handleFinishLoading}
         />
       );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    }
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this 
