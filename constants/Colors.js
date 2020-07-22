1acd061c3b50ce95b250d4bb4dbc87acb05e06b9
meticulu <113519094+meticulu@users.noreply.github.com>
2020-07-22 08:13:18 +0000

    Adjusted README

--- constants/Colors.js
+++ constants/Colors.js
@@ -5,4 +5,10 @@ export default {
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
-  erro
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
