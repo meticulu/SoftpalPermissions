dcfa17f0cddd1bb777b644e63ba4f1ee9ccb40cc
meticulu <113519094+meticulu@users.noreply.github.com>
2022-12-11 09:20:37 +0000

    Fix defaults

--- screens/Recorder.js
+++ screens/Recorder.js
@@ -24,4 +24,36 @@ class Icon {
 }
 
 const ICON_RECORD_BUTTON = new Icon(
-  requi
+  require('../assets/images/record_button.png'),
+  70,
+  119
+);
+const ICON_RECORDING = new Icon(
+  require('../assets/images/record_icon.png'),
+  20,
+  14
+);
+
+const ICON_PLAY_BUTTON = new Icon(
+  require('../assets/images/play_button.png'),
+  34,
+  51
+);
+const ICON_PAUSE_BUTTON = new Icon(
+  require('../assets/images/pause_button.png'),
+  34,
+  51
+);
+const ICON_STOP_BUTTON = new Icon(
+  require('../assets/images/stop_button.png'),
+  22,
+  22
+);
+
+const ICON_MUTED_BUTTON = new Icon(
+  require('../assets/images/muted_button.png'),
+  67,
+  58
+);
+const ICON_UNMUTED_BUTTON = new Icon(
+  require('../assets/images/unmuted_but
