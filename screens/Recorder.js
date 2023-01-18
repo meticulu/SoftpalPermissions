d058f1efbacb8bba16774c6a5a18f72f1e2eb3ae
meticulu <113519094+meticulu@users.noreply.github.com>
2023-01-18 09:36:38 +0000

    new png jp

--- screens/Recorder.js
+++ screens/Recorder.js
@@ -100,4 +100,23 @@ export default class App extends React.Component {
     const RECORDING_OPTIONS_PRESET_LOW_QUALITY = {
       android: {
         extension: '.m4a',
-        outputFormat: Audio.RECORDING_OPTION_A
+        outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+        audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
+        sampleRate: 44100,
+        numberOfChannels: 2,
+        bitRate: 128000,
+      },
+      ios: {
+        extension: '.wav',
+        audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+        sampleRate: 44100,
+        numberOfChannels: 1,
+        bitRate: 128000,
+        linearPCMBitDepth: 16,
+        linearPCMIsBigEndian: false,
+        linearPCMIsFloat: false,
+      },
+    };
+
+    this.recordingSettings = JSON.parse(
+      JSON.stringify(RECORDING_OPTIONS_PRESET_LOW_QUALITY)
